03de4398ac0003d5fa762007aeac4b11fc46eb7b
Adriano Santos <adriradiologista@hotmail.com.br>
2022-11-03 13:38:28 -0300

    Configurando sistema de login e senha com JWT.

--- src/main/java/com/example/springboot/controllers/UserController.java
+++ src/main/java/com/example/springboot/controllers/UserController.java
@@ -5,20 +5,18 @@ import com.example.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Classe responsável em disponibilizar um recurso HTTP para cadastrar um usuário.
  */
 @RestController
-@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping
+    @PostMapping("/users")
     public void postUser(@RequestBody UserModel userModel) {
         userService.createUser(userModel);
     }
